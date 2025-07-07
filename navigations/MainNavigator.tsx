25a1ba9fc8c112ae3a5da320a418968a562073f3
Aldair-Og <aldairendara5@gmail.com>
2025-07-07 00:27:58 -0500

    DEFINITIVO

--- navigations/MainNavigator.tsx
+++ navigations/MainNavigator.tsx
@@ -1,13 +1,8 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 
-//COMPONENTES
-import GuardarScreen from "../screens/GuardarScreen";
-import LeerScreen from "../screens/LeerScreen";
-import EditarScreen from "../screens/EditarScreen";
-import EliminarScreen from "../screens/EliminarScreen";
 import HomeScreen from "../screens/HomeScreen";
-import CargarProductosScreen from "../screens/CargarDatosScreen";
+
 import CatalogoScreen from "../screens/CatalogoScreen";
 import AgregarProductoScreen from "../screens/AgregarProductoScreen";
 
@@ -18,11 +13,6 @@ function MyStack() {
     return (
         <Stack.Navigator>
             <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="Guardar" component={GuardarScreen} />
-            <Stack.Screen name="Leer" component={LeerScreen} />
-            <Stack.Screen name="Editar" component={EditarScreen} />
-            <Stack.Screen name="Eliminar" component={EliminarScreen} />
-            <Stack.Screen name="CargarProductos" component={CargarProductosScreen} />
             <Stack.Screen name="Catalogo" component={CatalogoScreen} />
             <Stack.Screen name="AgregarProducto" component={AgregarProductoScreen} />
 
